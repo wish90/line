031a09e326a94eeb6cef32a25c89fca23b5ab15a
wish90 <121218170+wish90@users.noreply.github.com>
2022-12-22 15:37:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 22:05:00 2020
+
+@author: toocool
+"""
+
+import json
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+app = Flask(__name__)
+
+
+line_bot_api = LineBotApi('edFUkQybndZah161EJSfCNEhnNbwrS92WB2W8w+/uSZJiI+U+u5Ylfw7rBT32kplCQ3DahfN3+LdayHLDOMwJ0QSMZkHxRhdUo6A3Im9+R1xq3XuvB65DdH9dLUPSuiimB7cJA9G6qxdt+s8ZpjThQdB04t89/1O/w1cDnyilFU=')
+
+handler = WebhookHandler('35a3d3842e1a93e4ae1d5761eb71a854')
+
+line_bot_api.push_message('U1a447107797cfe0a2e2c8443df084569', TextSendMessage(text='OK,I am here ! May I help you? ｡^‿^｡'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+######################處理LINE USER 傳來得訊息 ###############################
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # get user id when reply
+    user_id = event.source.user_id
+    print("user_id =", user_id)
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+
+###############################################################################
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 27017))
+    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run()
